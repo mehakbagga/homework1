134a42327f36f89a0d347eb2dfd83c05b9f36602
mehak <mehakbagga55@gmail.com>
2022-06-21 17:24:05 -0700

    completed assignment

---
+++ turtleGraphics.js
@@ -0,0 +1,32 @@
+class Turtle{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+    
+    forward(num){
+        num = this.x
+    }
+
+    right(){
+        let angel = degToRad(90)
+        return turtle.angel += angel
+    }
+
+    left(){
+        let angel = degToRad(90)
+        return turtle.angel -= angel
+    }
+
+    allPoints(){
+
+    }
+
+    print(){
+
+    }
+
+    
+}
+
+var turtle = new Turtle()
